cf204df6ea4cd202f0f7ecf00f5f1b437dd5a6aa
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-09-17 23:47:53 +0700

    merger từ branch KietHoang

--- ChoTotUI/components/ModalPicker.js
+++ ChoTotUI/components/ModalPicker.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import {StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView} from 'react-native'
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-import {Citys_VN, Citys} from '../utils/data';
-
-import {change_alias} from '../utils/function';
+import { Cities_VN, Cities } from '../utils/data';
+import { change_alias } from '../utils/function';
 
 export default class ModalPicker extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             valueSearch: '',
@@ -21,33 +19,35 @@ export default class ModalPicker extends React.Component {
         });
     };
 
-
-
     render() {
         const input = change_alias(this.state.valueSearch.toLowerCase());
-        const option = Citys_VN.filter((city, index) => {return Citys[index].indexOf(input) != -1;}).map( (city) => {
+        const option = Cities_VN.filter((city, index) => { return Cities[index].indexOf(input) != -1; }).map((city) => {
             return (
-                <TouchableOpacity onPress={() => this.props.choiceCity(city)} activeOpacity={0.5} style={styles.pickerItem} key={city}>
-                    <Text style={{marginLeft: 30}}>{city}</Text>
+                <TouchableOpacity
+                    onPress={() => this.props.choiceCity(city)}
+                    activeOpacity={0.5}
+                    style={styles.pickerItem} key={city}
+                >
+                    <Text style={{ marginLeft: 30 }}>{city}</Text>
                 </TouchableOpacity>
             )
         })
-        
+
         return (
             <View style={styles.container}>
                 <View style={styles.searchContainer}>
-                    <TextInput 
-                        style={styles.textSearch} 
+                    <TextInput
+                        style={styles.textSearch}
                         value={this.state.valueSearch}
                         placeholder={'Tìm vùng bạn ở'}
                         placeholderTextColor={'#555555'}
                         onChangeText={(city) => this.onChangeValueSearch(city)}
-                        />
+                    />
                     <TouchableOpacity>
                         <Ionicons name="md-search" size={32} color="black" />
                     </TouchableOpacity>
                 </View>
-                <ScrollView style={{width: '100%'}} >
+                <ScrollView style={{ width: '100%' }} >
                     {option}
                 </ScrollView>
             </View>
@@ -57,35 +57,34 @@ export default class ModalPicker extends React.Component {
 
 const styles = StyleSheet.create({
     pickerItem: {
-        backgroundColor: 'white', 
-        justifyContent: 'center', 
-        height: 60, 
-        borderBottomWidth: 0.5, 
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        height: 60,
+        borderBottomWidth: 0.5,
         borderColor: 'gray'
     },
 
     container: {
-        width: '90%', 
-        height: 470, 
-        backgroundColor: 'white', 
+        width: '80%',
+        height: 470,
+        backgroundColor: 'white',
         borderRadius: 10,
     },
 
     searchContainer: {
-        height:50, 
-        backgroundColor: '#FFD02C', 
-        flexDirection: 'row', 
-        justifyContent: 'space-around', 
-        alignItems: 'center', 
-        borderTopRightRadius: 10, 
+        height: 50,
+        backgroundColor: '#FFD02C',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderTopRightRadius: 10,
         borderTopLeftRadius: 10,
     },
 
     textSearch: {
-        height:'80%', 
-        width: '80%', 
-        borderColor: 'black', 
+        height: '80%',
+        width: '80%',
+        borderColor: 'black',
         borderBottomWidth: 1,
     },
 });
-  
