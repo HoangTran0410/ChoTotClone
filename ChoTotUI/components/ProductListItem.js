fbd6a7abc47123579611c4763e5e978442b88ea8
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-09-30 01:02:09 +0700

    thêm trường hợp props là thông tin chi tiết của ad

--- ChoTotUI/components/ProductListItem.js
+++ ChoTotUI/components/ProductListItem.js
@@ -1,43 +1,55 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 import { labelProductData } from '../utils/data';
+import ListLabels from '../components/ListLabels';
+import { number } from 'prop-types';
 
-export default class ProductListItem extends React.PureComponent {
+export default class ProductListItem extends PureComponent {
+    constructor(props) {
+        super(props)
+    }
 
     openDetail = () => {
-        this.props.navigation.navigate('DetailAd')
+        this.props.navigation.navigate('DetailAd', { 'item': this.props.item })
     }
 
     render() {
-        const item = this.props.item;
+        const { item, customWidth } = this.props;
+        const image = item.image || item.ad.thumbnail_image;
+        const subject = item.subject || item.ad.subject;
+        const number_of_images = item.number_of_images || item.ad.images.length;
+        const price_string = item.price_string || item.ad.price_string;
+        const date = item.date || item.ad.date;
+        const area_name = item.area_name || item.ad.area_name;
+
         return (
-            <TouchableOpacity style={[styles.shadow, styles.container]} onPress={this.openDetail}>
+            <TouchableOpacity
+                onPress={this.openDetail}
+                style={[styles.shadow, styles.container, customWidth ? { width: customWidth } : {}]}
+            >
                 <View style={styles.headerContainer}>
-                    <Image style={styles.image} source={{ uri: item.image }} key={item.list_id} resizeMode={'cover'} />
-                    <Text style={{ fontSize: 14, fontWeight: '500', margin: 5 }}>{item.subject}</Text>
+                    <Image style={styles.image} source={{ uri: image }} resizeMode={'cover'} />
+                    <Text style={{ fontSize: 14, margin: 5 }}>{subject}</Text>
                 </View>
-                <View style={styles.bodyContainer}>
-                    <Text style={{ color: 'red', fontSize: 14, fontWeight: '500' }}>{item.price_string}</Text>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <FontAwesome name="home" size={12} color='#ffbf17' />
-                        <Text style={{ fontSize: 12 }}>|</Text>
-                        <Text style={{ color: 'grey', fontSize: 11 }}>{item.date}</Text>
-                        {/* <Feather name='award' size={15} color='#ffbf17' />
+                <View style={styles.imageCountContainer}>
+                    <Text style={styles.imageCount}>{number_of_images}</Text>
+                </View>
+                <View style={{ padding: 5 }}>
+                    <View style={styles.bodyContainer}>
+                        <Text style={{ color: 'red', fontSize: 14, fontWeight: '500' }}>{price_string}</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', margin: 3 }}>
+                            <FontAwesome name="home" size={12} color='#ffbf17' />
+                            <Text style={{ fontSize: 12 }}>|</Text>
+                            <Text style={{ color: 'grey', fontSize: 11 }}>{date}</Text>
+                            {/* <Feather name='award' size={15} color='#ffbf17' />
                         <Text style={{color: 'grey', fontSize: 13}}>Tin uu tien</Text> */}
-                        <Text style={{ fontSize: 12 }}>|</Text>
-                        <Text style={{ color: 'grey', fontSize: 11 }}>{item.area_name}</Text>
+                            <Text style={{ fontSize: 12 }}>|</Text>
+                            <Text style={{ color: 'grey', fontSize: 11 }}>{area_name}</Text>
+                        </View>
                     </View>
-                </View>
-                <View style={styles.labelContainer}>
-                    {labelProductData.map(item => {
-                        return (
-                            <View key={item.id} style={[styles.labelItem, { backgroundColor: item.color, }]}>
-                                <Text style={styles.labelText}>{item.title}</Text>
-                            </View>
-                        );
-                    })}
+                    <ListLabels listLabels={labelProductData} limit={2} />
                 </View>
             </TouchableOpacity>
         );
@@ -46,7 +58,6 @@ export default class ProductListItem extends React.PureComponent {
 
 const styles = StyleSheet.create({
     container: {
-        // height: 260,
         margin: 5,
         flex: 1,
         backgroundColor: 'white',
@@ -54,25 +65,16 @@ const styles = StyleSheet.create({
     },
 
     headerContainer: {
-        // flex: 0.5,
-        // margin: 5,
-    },
 
-    labelContainer: {
-        // flex: 0.275,
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        flexDirection: 'row',
     },
 
     bodyContainer: {
-        // flex: 0.225,
         marginLeft: 5,
         justifyContent: 'center',
     },
 
     image: {
-        height: 120,
+        height: 150,
         width: '100%',
     },
 
@@ -80,24 +82,29 @@ const styles = StyleSheet.create({
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 2,
+            height: 5,
         },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
+        shadowOpacity: 0.4,
+        shadowRadius: 3,
 
-    labelItem: {
-        height: 18,
-        margin: 2.5,
-        alignItems: 'center',
+        elevation: 7,
+    },
+    imageCountContainer: {
+        position: 'absolute',
+        top: 5,
+        right: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        backgroundColor: '#0005',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#ddda',
         justifyContent: 'center',
-        borderRadius: 4,
+        alignItems: 'center'
     },
-
-    labelText: {
-        marginHorizontal: 5,
-        fontSize: 12,
+    imageCount: {
+        color: '#fff',
+        fontWeight: 'bold'
     },
 });
 
