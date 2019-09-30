44fb5249c985f1740eca081bc08ffcd81c417c41
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-09-30 22:30:02 +0700

    các hàm làm việc với date_time

--- ChoTotUI/utils/functions.js
+++ ChoTotUI/utils/functions.js
@@ -14,7 +14,7 @@ function change_alias(alias) {
     str = str.replace(/ + /g, " ");
     str = str.trim();
     return str;
-};
+}
 
 const dialCall = (phone) => {
     let phoneNumber = '';
@@ -25,7 +25,70 @@ const dialCall = (phone) => {
         phoneNumber = 'telprompt:${' + phone + '}';
     }
     Linking.openURL(phoneNumber);
-};
+}
 
 
-export { change_alias, dialCall };
+// ============================ DATE TIME FUNCTIONS =======================
+function appendLeadingZeroes(n) {
+    if (n <= 9) {
+        return "0" + n;
+    }
+    return n
+}
+
+function formatDate(date, type) {
+    let d = appendLeadingZeroes(date.getDate())
+    let m = appendLeadingZeroes(date.getMonth() + 1)
+    let y = appendLeadingZeroes(date.getFullYear())
+
+    let h = appendLeadingZeroes(date.getHours())
+    let mi = appendLeadingZeroes(date.getMinutes())
+    let s = appendLeadingZeroes(date.getSeconds())
+
+    if (type == 'dd/mmm/yyyy')
+        return `${d}/${m}/${y}`
+    if (type == 'dd/mmm/yyyy hh:mm:ss')
+        return `${d}/${m}/${y} ${h}:${mi}:${s}`
+}
+
+function secsToDateTime(secs) {
+    var t = new Date(1970, 0, 1); // Epoch
+    t.setSeconds(secs);
+    return t;
+}
+
+function msToTime(duration) {
+    return {
+        hours: Math.floor((duration / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((duration / (1000 * 60)) % 60),
+        seconds: Math.floor((duration / 1000) % 60)
+    }
+}
+
+function responseTimeText(duration) {
+    let hours = Math.floor((duration / (60 * 60)) % 24);
+    let minutes = Math.floor((duration / 60) % 60);
+    let seconds = Math.floor(duration % 60);
+
+    if (hours > 0) return `Trong ${hours + (minutes > 30 ? 1 : 0)} giờ`
+    if (minutes > 0) return `Trong ${minutes + (seconds > 30 ? 1 : 0)} phút`
+    return 'Dưới 1 phút'
+}
+
+function calculateOnlineTime(online_time) {
+    let online = secsToDateTime(online_time);
+    let diff = new Date() - online;
+
+    let diffStr = msToTime(diff);
+    const { hours, minutes, seconds } = diffStr;
+
+    if (hours > 0) {
+        return hours + ' giờ trước';
+    }
+    if (minutes > 0) {
+        return minutes + ' phút trước';
+    }
+    return 'Vài giây trước';
+}
+
+export { change_alias, dialCall, responseTimeText, calculateOnlineTime };
