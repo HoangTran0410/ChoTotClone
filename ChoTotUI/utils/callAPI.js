bba800cd50fe04b4ad4e99ccf48702381fa735e3
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2019-10-03 03:50:29 +0700

    try catch, thêm api

--- ChoTotUI/utils/callAPI.js
+++ ChoTotUI/utils/callAPI.js
@@ -24,116 +24,149 @@ import { Alert } from 'react-native';
 const apiUrl = 'https://gateway.chotot.com/v1/public/'
 const apiUrl_v2 = 'https://gateway.chotot.com/v2/public/'
 
-const getListAds = async ({ page, region_v2 = 13000, cg, giveaway }) => {
-  try {
-    let limit = 30;
-    let link = `${apiUrl}ad-listing?region_v2=${region_v2}&limit=${limit}&o=${(page - 1) * limit}&page=${page}`;
-    if (cg) link += ('&cg=' + cg);
-    if (giveaway) link += '&giveaway=true'
+const getListAds = async ({ page, region_v2 = 13000, cg, giveaway }, callBack) => {
+	let jsonData;
+	try {
+		let limit = 30;
+		let link = `${apiUrl}ad-listing?region_v2=${region_v2}&w=1&limit=${limit}&o=${(page - 1) * limit}&page=${page}`;
+		if (cg) link += ('&cg=' + cg);
+		if (giveaway) link += '&giveaway=true';
+		else link += '&st=s';
 
-    const response = await fetch(link)
-    const jsonData = await response.json();
+		const response = await fetch(link)
+		jsonData = await response.json();
 
-    return jsonData;
+	} catch (e) {
+		Alert.alert('Lỗi lấy danh sách tin đăng', e.message);
+		return null;
+	}
 
-  } catch (e) {
-    Alert.alert('Lỗi lấy danh sách tin đăng', e.message);
-    return null;
-  }
+	if (callBack)
+		callBack(jsonData)
+
+	return jsonData;
 }
 
 const getDetailAd = async (id, callBack) => {
-  let jsonData;
+	let jsonData;
 
-  try {
-    const response = await fetch(`${apiUrl}ad-listing/${id}`);
-    jsonData = await response.json();
+	try {
+		const response = await fetch(`${apiUrl}ad-listing/${id}`);
+		jsonData = await response.json();
 
-  } catch (e) {
-    Alert.alert('Lỗi lấy dữ liệu tin đăng', e.message);
-    return null;
-  }
+	} catch (e) {
+		Alert.alert('Lỗi lấy dữ liệu tin đăng', e.message);
+		return null;
+	}
 
-  if (callBack) {
-    callBack(jsonData)
-  }
+	if (callBack) {
+		callBack(jsonData)
+	}
 
-  return jsonData;
+	return jsonData;
 }
 
 const getAccountInfo = async (oid, callBack) => {
-  let result;
+	let result;
 
-  try {
-    const info = await fetch(`${apiUrl}profile/${oid}`);
-    const infoData = await info.json();
+	try {
+		// const urls = [
+		// 	`${apiUrl}profile/${oid}`,
+		// 	`${apiUrl_v2}chat/user/get/${oid}`,
+		// 	`${apiUrl}ratings/${oid}?post_type=all`
+		// ]
 
-    const chat = await fetch(`${apiUrl_v2}chat/user/get/${oid}`);
-    const chatData = await chat.json();
+		// Promise.all(urls.map(url =>
+		// 	fetch(url).then(resp => resp.json())
+		// )).then(jsons => {
+		// 	result = {
+		// 		info: jsons[0],
+		// 		chat: jsons[1],
+		// 		rating: jsons[2]
+		// 	}
+		// })
+		const info = await fetch(`${apiUrl}profile/${oid}`);
+		const infoData = await info.json();
 
-    const rating = await fetch(`${apiUrl}ratings/${oid}?post_type=all`);
-    const ratingData = await rating.json();
+		const chat = await fetch(`${apiUrl_v2}chat/user/get/${oid}`);
+		const chatData = await chat.json();
 
-    result = {
-      info: infoData,
-      chat: chatData,
-      rating: ratingData
-    }
+		const rating = await fetch(`${apiUrl}ratings/${oid}?post_type=all`);
+		const ratingData = await rating.json();
 
-  } catch (e) {
-    Alert.alert('Lỗi lấy dữ liệu người đăng', e.message);
-    return null;
-  }
+		result = {
+			info: infoData,
+			chat: chatData,
+			rating: ratingData
+		}
 
-  // if (callBack) {
-  //   callBack(result)
-  // }
+	} catch (e) {
+		Alert.alert('Lỗi lấy dữ liệu người đăng', e.message);
+		return null;
+	}
 
-  return result;
+	if (callBack) {
+		callBack(result)
+	}
+
+	return result;
 }
 
 const getRecommends = async (item, callBack) => {
 
-  let jsonData;
+	let jsonData;
 
-  try {
-    const response = await fetch('http://192.168.1.156:5000/recommendation-system', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(item)
-    })
+	try {
+		const response = await fetch('http://192.168.1.78:5000/recommendation-system', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: "POST",
+			body: JSON.stringify(item)
+		})
 
-    jsonData = await response.json();
-  } catch (e) {
-    Alert.alert('Lỗi lấy dữ liệu recommends', e.message);
-    return null;
-  }
+		jsonData = await response.json();
 
-  // if (callBack) {
-  //   callBack(jsonData)
-  // }
+	} catch (e) {
+		Alert.alert('Lỗi lấy dữ liệu recommends', e.message);
+		return null;
+	}
 
-  return jsonData;
+	if (callBack) {
+		callBack(jsonData)
+	}
+
+	return jsonData;
 }
 
 const getListBanners = async () => {
-  const response = await fetch(`${apiUrl}/buyer-collection/banners`)
-  const data = await response.json();
-  // console.log('get', data);
-  return data;
+	const response = await fetch(`${apiUrl}/buyer-collection/banners`)
+	const data = await response.json();
+	// console.log('get', data);
+	return data;
 }
 
-const beautifi_itemData = () => {
+const readableItem = (item) => {
+	let ad = {};
 
+	if (item.ad) {
+		ad = { ...item.ad }
+	} else {
+		ad = { ...item }
+	}
+
+	ad.number_of_images = ad.number_of_images || (ad.images ? ad.images.length : 0);
+	ad.image = ad.image || ad.thumbnail_image;
+
+	return ad
 }
 
 export {
-  // loginUser,
-  getListAds,
-  getDetailAd,
-  getListBanners,
-  getAccountInfo,
-  getRecommends
+	// loginUser,
+	getListAds,
+	getDetailAd,
+	getListBanners,
+	getAccountInfo,
+	getRecommends,
+	readableItem
 }
